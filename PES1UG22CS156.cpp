b52d7b4696b15454002884e72ff8718c4355569f
chetan-914 <124229171+chetan-914@users.noreply.github.com>
2025-03-11 16:51:46 +0530

    Update PES1UG22CS156.cpp

--- PES1UG22CS156.cpp
+++ PES1UG22CS156.cpp
@@ -1,10 +1,6 @@
-//File: hello.cpp
 #include <iostream>
-using namespace std;
 
-int main(void) {
-  cout << "Hello, World\n";
-  cout << "Hello, Jenkins\n";
-  cout << "I have successfully built and run this. \n" << "jenkins is working ", << "creating cpp file";
-  return 0;
+int main() {
+    std::cout << "Hello, Jenkins Pipeline!" << std::endl;
+    return 0;
 }
